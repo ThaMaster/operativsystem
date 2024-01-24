1c765c9f6dba0fe486b37184e18e77a400ea675f
Christoffer Nordlander <christoffernordlander02@gmail.com>
2024-01-24 12:42:57 +0100

    Made it possible to communicate with hello_ioctl module.

---
+++ hello_ioctl/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+
+
+#define WR_VALUE _IOW('a','a', int32_t *)
+#define RD_VALUE _IOR('a','b', int32_t *)
+
+int main(void) 
+{
+    int fd;
+    printf("\n Opening Driver\n");
+    fd = open("/dev/etx_device", O_RDWR);
+    if(fd < 0) {
+        printf("Cannot open device file...\n");
+        return 0;
+    }
+
+    printf("Writing to file\n");
+    int32_t number = 255;
+    ioctl(fd, WR_VALUE, (int32_t *)&number);
+    
+    printf("Reading to file\n");
+    int32_t value;
+    ioctl(fd, RD_VALUE, (int32_t *)&value);
+
+    printf("Closing Driver\n");
+    close(fd);
+    
+    return 0;
+}
